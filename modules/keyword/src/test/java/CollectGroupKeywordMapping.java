beea010873a82868dc08c956a03d9cf61bfaf8af
macle86 <ysys86a@gmail.com>
2023-02-28 04:41:09 +0900

    2023

---
+++ modules/keyword/src/test/java/CollectGroupKeywordMapping.java
@@ -0,0 +1,28 @@
+import com.seomse.commons.config.Config;
+import com.seomse.jdbc.JdbcQuery;
+import com.seomse.jdbc.objects.JdbcObjects;
+import io.runon.commons.keyword.CollectKeywordMap;
+
+import java.util.List;
+
+/**
+ * @author macle
+ */
+public class CollectGroupKeywordMapping {
+    public static void main(String[] args) {
+        Config.getConfig("");
+
+        String collectId ="naver_cafe_search_crawling";
+
+        String groupId = "insurance";
+
+        List<String> list = JdbcQuery.getStringList("select keyword_no from collect_group_keyword_map where collect_group_id='" + groupId +"'");
+
+        for(String no : list){
+            CollectKeywordMap map = new CollectKeywordMap();
+            map.setCollectId(collectId);
+            map.setKeywordNo(Long.parseLong(no));
+            JdbcObjects.insertIfNoData(map);
+        }
+    }
+}
