a71f2c4077f813d649c7e976a3c8dd2b54db3709
macle <ysys86a@gmail.com>
2024-03-20 12:54:32 +0900

    2024

--- modules/install/src/main/java/io/runon/install/WindowsConfigure.java
+++ modules/install/src/main/java/io/runon/install/WindowsConfigure.java
@@ -10,10 +10,14 @@ import java.nio.file.Paths;
  * 윈도우 설치환경 자동세팅
  * @author macle
  */
+@SuppressWarnings("CallToPrintStackTrace")
 public class WindowsConfigure {
     public static void main(String[] args) {
-        JavaClassPathOut.out();
-
+        try {
+            JavaClassPathOut.out();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         Path currentPath = Paths.get("");
         String path = currentPath.toAbsolutePath().toString();
 
@@ -24,41 +28,50 @@ public class WindowsConfigure {
 
         String javaPath = args[0];
 
-        for(File file : files){
-            if(file.isDirectory()){
-                continue;
-            }
-            if(file.getName().equals("env.bat")){
-                continue;
-            }
-            if(file.getName().endsWith(".bat")){
-                String batText = FileUtils.getFileContents(file, "EUC-KR");
-                StringBuilder sb = new StringBuilder();
+        try {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    continue;
+                }
+                if (file.getName().equals("env.bat")) {
+                    continue;
+                }
+                if (file.getName().endsWith(".bat")) {
+                    String batText = FileUtils.getFileContents(file, "EUC-KR");
+                    StringBuilder sb = new StringBuilder();
 
-                String [] lines = batText.split("\n");
-                for(String line: lines){
+                    String[] lines = batText.split("\n");
+                    for (String line : lines) {
 
-                    sb.append("\n");
-                    if(line.startsWith("SET APP_HOME")){
-                        sb.append("SET APP_HOME=\"").append(path).append("\"");
-                    }else if(line.startsWith("SET JAVA_PATH")){
-                        sb.append("SET JAVA_PATH=\"").append(javaPath).append("\"");
-                    }else{
-                        sb.append(line);
+                        sb.append("\n");
+                        if (line.startsWith("SET APP_HOME")) {
+                            sb.append("SET APP_HOME=\"").append(path).append("\"");
+                        } else if (line.startsWith("SET JAVA_PATH")) {
+                            sb.append("SET JAVA_PATH=\"").append(javaPath).append("\"");
+                        } else {
+                            sb.append(line);
+                        }
+                    }
+
+                    if (sb.length() > 0) {
+                        FileUtils.fileOutput(sb.substring(1), file.getAbsolutePath(), "EUC-KR", false);
+                        sb.setLength(0);
                     }
                 }
-
-                if(sb.length() > 0){
-                    FileUtils.fileOutput(sb.substring(1),file.getAbsolutePath(),"EUC-KR",false);
-                    sb.setLength(0);
-                }
             }
+        }catch (Exception e){
+            e.printStackTrace();
         }
-
-        ConfigRelativePath configRelativePath = new ConfigRelativePath(path);
-        configRelativePath.change();
-
-        new LogbackHomeDir(path).change();
-
+        try {
+            ConfigRelativePath configRelativePath = new ConfigRelativePath(path);
+            configRelativePath.change();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            new LogbackHomeDir(path).change();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
