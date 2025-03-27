3acffd7a130883322cd5f26c9672ba54c315c457
macle <ysys86a@gmail.com>
2025-03-28 01:05:52 +0900

    2025

--- modules/commons/src/main/java/io/runon/commons/math/MathAdds.java
+++ modules/commons/src/main/java/io/runon/commons/math/MathAdds.java
@@ -58,4 +58,36 @@ public class MathAdds {
         return sum/valueArray.length;
     }
 
+    public static int levenshteinDistance(char[] source, char [] target) {
+
+
+        int n = source.length;
+        int m = target.length;
+        // 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
+        int[][] dp = new int[n + 1][m + 1];
+        // DP 테이블 초기 설정
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = j;
+        }
+
+
+        // 최소 편집 거리 계산
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // 문자가 같다면, 왼쪽 위에 해당하는 수를 그대로 대입
+                if (source[i - 1] == target[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // 문자가 다르다면, 세 가지 경우 중에서 최솟값 찾기
+                    // 삽입(왼쪽), 삭제(위쪽), 교체(왼쪽 위) 중에서 최소 비용을 찾아 대입
+                    dp[i][j] = 1 + Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
 }
